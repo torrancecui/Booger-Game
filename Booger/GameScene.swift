37fa02d22c2016091e829c558e449b1dc57a113b
Torrance Cui <cuitorrance@gmail.com>
2020-07-06 15:11:31 -0700

    added functioning physics

--- Booger/GameScene.swift
+++ Booger/GameScene.swift
@@ -11,8 +11,23 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball = SKSpriteNode()
+    var player = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        player = self.childNode(withName: "playerPaddle") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemyPaddle") as! SKSpriteNode
         
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy:20))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
     }
     
     override func update(_ currentTime: TimeInterval){
